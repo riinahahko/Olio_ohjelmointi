93b9161a03ac65e10f7d616f0da1697f42f937f1
riinahahko <114108520+riinahahko@users.noreply.github.com>
2023-02-01 09:57:15 +0200

    Create Exercise_2.py
    Task 3 in Exercise 2

---
+++ Exercise_2.py
@@ -0,0 +1,38 @@
+# File: Exercise_2.py
+# Author: Riina Hahko
+# Description: Programming demo
+
+
+
+def main(): 
+
+    while True:
+        user_input = input("Exercise points: ")
+
+        try:
+            point = int(user_input)
+
+            if point >= 0 and point <= 120:
+                
+                if point >= 50 and point <= 64:
+                    print("Your grade is: 1")
+                elif point >= 65 and point <= 78:
+                    print("Your grade is: 2")
+                elif point >= 79 and point <= 92:
+                    print("Your grade is: 3")
+                elif point >= 93 and point <= 106:
+                    print("Your grade is: 4")
+                elif point >= 107 and point <= 120:
+                    print("Your grade is: 5")
+                elif point < 50:
+                    print("Your grade is: No grade") 
+                break
+
+            else:
+                print("Error: exercise points cannot be <0 or >120.")
+        
+        except ValueError:
+            print("Error: exercise points cannot be <0 or >120.")
+        
+
+main()
